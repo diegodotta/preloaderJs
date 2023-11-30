97f91103765ef2f269f9368ad013bcdc555c51bd
Diego Dotta <diegodotta@gmail.com>
2023-11-30 10:12:48 -0800

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://diegodotta.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://diegodotta.github.io/preloaderJs/loading_bike2.gif';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
